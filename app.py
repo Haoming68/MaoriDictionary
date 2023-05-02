e378edd98ff24dc397171dd778eb825e7293fb79
68 <xianghaoming68@gmail.com>
2023-05-03 10:14:14 +1200

    1

--- app.py
+++ app.py
@@ -8,9 +8,11 @@ DATABASE = "C:/Users/68/Desktop/Maori Dictionary/maori.db"
 app = Flask(__name__)
 
 @app.route('/')
-def render_homepage():  # put application's code here
+def render_home_page():  # put application's code here
     return render_template('home.html')
 
-
+@app.route('/login', methods=['POST', 'GET'])
+def render_login_page():  # put application's code here
+    return render_template('login.html')
 
 app.run(host='0.0.0.0',debug=True)
